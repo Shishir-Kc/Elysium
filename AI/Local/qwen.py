785601d33b52e6ffa151f2996daf14ccfb0fc821
Shishir-Kc <shishir.khatri.kc@gmail.com>
2026-03-16 19:31:21 +0545

    added ome des

--- AI/Local/qwen.py
+++ AI/Local/qwen.py
@@ -1,23 +1,31 @@
+from pickle import TRUE
 from langchain_ollama.chat_models import ChatOllama
 
 model = ChatOllama(
     model="qwen3.5:0.8b",
-    reasoning=True,
+    reasoning=False,
 )
-def chat():
- for chat in model.stream(" write a py code to print hello world form ml model !  "):
-    try:
-     reasoning =chat.additional_kwargs['reasoning_content']
-    except:
-     reasoning=None
 
-    if reasoning:
-        yield ("Reasoning",reasoning)
-    content = chat.content
-    yield ("Answer",content) 
 
-for type,text in chat():
-   if type == "Reasoning":
-    print(f"\033[90m{text}\033[0m", end="") 
+def chat(msg: str):
+    for chat in model.stream(msg):
+        try:
+            reasoning = chat.additional_kwargs["reasoning_content"]
+        except:
+            reasoning = None
 
-   print(text,end="",flush=True)
+        if reasoning:
+            yield ("Reasoning", reasoning)
+        content = chat.content
+        yield ("Answer", content)
+
+
+while True:
+    for type, text in chat(msg=input(":>")):
+        if type == "Reasoning":
+            print(f"{text}", end="")
+
+        print(text, end="", flush=True)
+
+# while True:
+#     chat(msg=input(":>"))
