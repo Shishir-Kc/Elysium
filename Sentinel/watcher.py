1d3b81d634fb4ee6318eea7ca9dc1edd5300f0a9
Shishir-Kc <shishir.khatri.kc@gmail.com>
2026-03-27 17:20:57 +0545

    added Sentinel to watch vover Elyium files security and monitor it

--- Sentinel/watcher.py
+++ Sentinel/watcher.py
@@ -1,25 +1,63 @@
 import hashlib
 import os
 import json
-
 import time
 from pathlib import Path
 import shutil
+import sys
 
-BASE_DIR = Path(__file__).resolve().parent.parent
-os.makedirs(f"{BASE_DIR}/Elysium_back_up", exist_ok=True)
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-shutil.copy(f"{BASE_DIR}/main.py", f"{BASE_DIR}/Elysium_back_up/")
+from Tools.Progress_bar.smooth_bar import smooth_progress
 
 
 class Sentinel:
     def __init__(self) -> None:
         self.ignore_files = []
+        self.BASE_DIR = Path(__file__).resolve().parent.parent
+        self.BACK_UP_DIR = f"{self.BASE_DIR}/Elysium_back_up"
+        self.hasher = hashlib.sha256()
+        self.Changed_files = []
+        os.makedirs(
+            f"{self.BASE_DIR}/Elysium_back_up", exist_ok=True
+        )  # makes a dir named Elysium_back_up so that the current state of the code is saved in back up dir!
+        self.current_hash = {}
+        self.runtime_hash = {}
+        self.is_run_time_hash = False
 
-    def compute_has(self, file_path):
+    def logo(self):
+        with open("assets/Watcher/eye.txt", "r") as f:
+            art = f.read()
+        return art
+
+    def hash_file(self, file_path):
+        # print(file_path)
+        self.hasher = hashlib.sha256()
         with open(file_path, "rb") as f:
-            file = f.read()
-        return hashlib.sha256(file).hexdigest()
+            while chunk := f.read(8192):
+                self.hasher.update(chunk)
+
+        return self.hasher.hexdigest()
+
+    def hash_directory(self, directory):
+        self.hasher = hashlib.sha256()
+        for root, dirs, files in os.walk(directory):
+            dirs[:] = [d for d in dirs if d not in self.ignore_files]
+
+            for name in sorted(files):
+                if name in self.ignore_files:
+                    continue
+
+                filepath = os.path.join(root, name)
+
+                rel_path = os.path.relpath(filepath, directory)
+                self.hasher.update(rel_path.encode())
+
+                with open(filepath, "rb") as f:
+                    while chunk := f.read(8192):
+                        self.hasher.update(chunk)
+
+        return self.hasher.hexdigest()
 
     def look_dir(self):
         with open(f"{os.path.dirname(os.path.abspath(__file__))}/dir.json", "r") as f:
@@ -37,29 +75,73 @@ class Sentinel:
             self.ignore_files.append(i)
         return self.ignore_files
 
+    def create_hash(self):
+        for directory in os.listdir(self.BASE_DIR):
+            full_dir = os.path.join(self.BASE_DIR, directory)
+            if os.path.basename(full_dir) in self.ignore_files:
+                continue
+            if not self.is_run_time_hash:
+                try:
+                    if os.path.isfile(full_dir):
+                        self.runtime_hash[full_dir] = self.hash_file(full_dir)
+                    if os.path.isdir(full_dir):
+                        self.runtime_hash[full_dir] = self.hash_directory(full_dir)
 
-sentinel = Sentinel()
-base_hash = sentinel.compute_has(f"{BASE_DIR}/main.py")
+                except Exception as e:
+                    print(f"got an Error ! {e} {directory}")
+            try:
+                if os.path.isfile(full_dir):
+                    self.current_hash[full_dir] = self.hash_file(full_dir)
+                if os.path.isdir(full_dir):
+                    self.current_hash[full_dir] = self.hash_directory(full_dir)
+            except Exception as e:
+                print(f"got an Error ! {e} {directory}")
+        else:
+            self.is_run_time_hash = True
 
-print(sentinel.load_ignore_file_names())
+    def create_back_up(self):
+        smooth_progress(text="Creating Back Up ")
+        for items in os.listdir(self.BASE_DIR):
+            # print(items)
+            src_path = os.path.join(self.BASE_DIR, items)
+            destination_path = os.path.join(sentinel.BACK_UP_DIR, items)
+
+            if os.path.basename(src_path) in sentinel.ignore_files:
+                continue
+
+            if os.path.isdir(src_path):
+                try:
+                    shutil.copytree(
+                        src_path,
+                        destination_path,
+                        dirs_exist_ok=True,
+                    )
+                except Exception as e:
+                    print(f"got an error ! {e}")
+            else:
+                shutil.copy2(src_path, destination_path)
+
+    def watch(self):
+
+        while True:
+            _ = self.create_hash()
+
+            if self.current_hash != self.runtime_hash:
+                print("change detected in file . . . . ")
+                print("Getting the file")
+                for file_path in self.current_hash:
+                    if self.current_hash[file_path] != self.runtime_hash[file_path]:
+                        self.Changed_files.append(file_path)
+                print("File(s) Changed at => ", self.Changed_files)
+                break
+            print("no change", end="\r", flush=True)
+            time.sleep(5)
 
 
-def main():
-    while True:
-        current_hash = sentinel.compute_has(f"{BASE_DIR}/main.py")
-        if current_hash != base_hash:
-            print("chage in file !")
-            print("Rebooting the server ! ")
-            break
-        time.sleep(5)
-
-
-source = BASE_DIR
-src_path = None
-
-
-for items in os.listdir(source):
-    # print(items)
-    src_path = os.path.join(source, items)
-    if os.path.basename(src_path) in sentinel.ignore_files:
-        print(f"found => {src_path}")
+if __name__ == "__main__":
+    sentinel = Sentinel()
+    print(sentinel.logo())
+    sentinel.load_ignore_file_names()
+    sentinel.create_hash()
+    sentinel.create_back_up()
+    sentinel.watch()
