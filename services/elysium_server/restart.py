1036d4a2660623875fdb67baca819a0d9026232d
Shishir-Kc <shishir.khatri.kc@gmail.com>
2026-03-28 17:26:52 +0545

    docs: add Sentinel docs and replace Python with uv in README

--- services/elysium_server/restart.py
+++ services/elysium_server/restart.py
@@ -4,30 +4,27 @@ import sys
 import time
 from Tools.Progress_bar.smooth_bar import smooth_progress
 import logging
+
 logger = logging.getLogger("uvicorn.errors")
 
-def restart_service():
-    logger.warning("Rebooting The server . . . ")  
-    with open('assets/Elysium/restarting.txt', 'r') as f:
-        restart_text = f.read()
 
+def restart_service():
+    logger.warning("Rebooting The server . . . ")
+    with open("assets/Elysium/restarting.txt", "r") as f:
+        restart_text = f.read()
 
     print(restart_text)
 
-    
     time.sleep(5)
-    os.system('clear')
+    os.system("clear")
 
     os.execv(sys.executable, [sys.executable] + sys.argv)
-    
+
+
 def restart_elysium():
 
     server_restart = threading.Thread(target=restart_service)
-    Smooth_bar = threading.Thread(
-    target=smooth_progress,
-    kwargs={"text": "Rebooting"}
-    )
-
+    Smooth_bar = threading.Thread(target=smooth_progress, kwargs={"text": "Rebooting"})
 
     server_restart.start()
     time.sleep(1)
@@ -35,5 +32,3 @@ def restart_elysium():
 
     server_restart.join()
     Smooth_bar.join()
-
-
